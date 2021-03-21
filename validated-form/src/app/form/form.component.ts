97a5f73670e721f7947a14c23622c50bc8f359cf
Ľudmila Jalčová <lu.jalcova@gmail.com>
2021-03-21 17:27:44 +0100

    update

--- validated-form/src/app/form/form.component.ts
+++ validated-form/src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -8,47 +8,85 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class FormComponent {
   isValidName = true;
   isValidPsswd = true;
+  isValidNewPsswd = true;
+  resetPassword = false;
+  isLoading = false;
+  isLogIn = false;
+  isChangedPassword = false;
 
-  userName: string = 'Feri';
-  userPsswd: string = '98765';
+  dbUserName: string = 'Feri';
+  dbUserPsswd: string = '98765';
+  newPsswd = '';
 
-  newName: string = '';
-  newPsswd: string = '';
+  credentialValidation(
+    name: HTMLInputElement,
+    psswd: HTMLInputElement,
+    newPsswd: any
+  ) {
+    this.isValidName = true;
+    this.isValidPsswd = true;
+    this.isValidNewPsswd = true;
+    this.isLoading = true;
 
-  @Output('name') name = new EventEmitter<string>();
-  @Output('psswd') psswd = new EventEmitter<string>();
-
-  nameValidation(value: string) {
-    if (this.userName.trim().toLowerCase() !== value) {
-      this.isValidName = false;
+    if (this.dbUserName.trim().toLowerCase() !== name.value) {
       console.log('Wrong name!');
       setTimeout(() => {
-        this.isValidName = true;
-      }, 3000);
+        this.isValidName = false;
+        this.isLoading = false;
+      }, 500);
     }
-    console.log(this.userName);
-  }
 
-  psswdValidation(value: string) {
-    if (this.userPsswd !== value) {
-      this.isValidPsswd = false;
+    if (this.dbUserPsswd !== psswd.value && !this.resetPassword) {
       console.log('Wrong psswd!');
       setTimeout(() => {
-        this.isValidPsswd = true;
-      }, 3000);
+        this.isValidPsswd = false;
+        this.isLoading = false;
+      }, 500);
     }
-    console.log(this.userPsswd);
+
+    if (
+      this.dbUserPsswd === newPsswd.value &&
+      this.resetPassword &&
+      newPsswd.value !== psswd.value
+    ) {
+      console.log('Wrong psswd!');
+      setTimeout(() => {
+        this.isValidNewPsswd = false;
+        this.isLoading = false;
+      }, 500);
+    }
+
+    if (
+      this.dbUserName.trim().toLowerCase() === name.value &&
+      this.dbUserPsswd === psswd.value &&
+      !this.resetPassword
+    ) {
+      setTimeout(() => {
+        this.isLoading = false;
+        this.isLogIn = true;
+      }, 500);
+    }
+    console.log(newPsswd, psswd.value);
+
+    if (
+      this.dbUserPsswd !== newPsswd.value &&
+      this.resetPassword &&
+      newPsswd.value === psswd.value
+    ) {
+      setTimeout(() => {
+        this.isLoading = false;
+        this.isChangedPassword = true;
+        this.dbUserPsswd = newPsswd.value;
+      }, 500);
+    }
   }
 
-  resetUserName() {
-    this.name.emit(this.newName);
-    this.userName = this.newName;
-    console.log(this.userName);
-  }
-
-  resetPassword() {
-    this.psswd.emit(this.newPsswd);
-    this.userPsswd = this.newPsswd;
-    console.log(this.userPsswd);
+  resetUser() {
+    this.resetPassword = true;
+    this.isValidName = true;
+    this.isValidPsswd = true;
+    this.isValidNewPsswd = true;
+    this.isLogIn = false;
+    this.isChangedPassword = false;
   }
 }
